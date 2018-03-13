9699136067b857b966076fddd26a69d28d1d5019
Chris Legault <chrislegault2011@gmail.com>
2018-03-13 15:40:56 -0300

    Heroku Setup - trying to get heroku wired up properly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+const logger = require("./logger");
 const Client = require("./client");
 const client = new Client();
-client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN).then(() => logger.info("Logged in"));
