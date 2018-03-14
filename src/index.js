7866bacc0583a9ff4eea82dd7d6b191b76cfdb7f
Chris Legault <chrislegault2011@gmail.com>
2018-03-14 09:29:34 -0300

    Client Tests (#42)
    - added tests for client
    - moved custom handlers out to own file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const logger = require("./logger");
 const Client = require("./client");
 const client = new Client();
+
 client
   .login(process.env.DISCORD_TOKEN)
   .then(() => logger.info("Client authenticated"));
+
+module.exports = client;
