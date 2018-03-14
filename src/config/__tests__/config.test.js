f14375e5736ce1306f31018da8fd5f13727d69fe
Chris Legault <chrislegault2011@gmail.com>
2018-03-13 22:42:33 -0300

    Heroku setup - attach winston logger to sequelize

--- src/config/__tests__/config.test.js
+++ src/config/__tests__/config.test.js
@@ -1,7 +1,24 @@
+jest.mock("../../logger", () => ({
+  info: jest.fn("info")
+}));
+
+const logger = require("../../logger");
 const config = require("../config");
 
 describe("config/config", () => {
   it("should have the proper database config", () => {
     expect(config).toMatchSnapshot();
+  });
+
+  it("should configure logging for development config", () => {
+    const msg = "my message";
+    config.development.logging(msg);
+    expect(logger.info).toHaveBeenCalledWith(msg);
+  });
+
+  it("should configure logging for production config", () => {
+    const msg = "my message";
+    config.production.logging(msg);
+    expect(logger.info).toHaveBeenCalledWith(msg);
   });
 });
--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,18 @@
+const logger = require("../logger");
+
 module.exports = {
   development: {
     dialect: "sqlite",
     storage: "lootbot.sqlite",
     operatorsAliases: false,
-    seederStorage: "sequelize"
+    seederStorage: "sequelize",
+    logging: msg => logger.info(msg)
   },
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     operatorsAliases: false,
-    seederStorage: "sequelize"
+    seederStorage: "sequelize",
+    logging: msg => logger.info(msg)
   }
 };
