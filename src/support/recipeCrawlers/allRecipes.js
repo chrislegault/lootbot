35009c31d87cf65deb18610090f39815db8e899e
Chris Legault <chrislegault2011@gmail.com>
2020-03-20 21:46:52 -0300

    New Instant Pot Recipes (#55)
    - add new source for recipes
    - cache recipes on first use of command instead of fetch on each run

---
+++ src/support/recipeCrawlers/allRecipes.js
@@ -0,0 +1,24 @@
+const x = require("x-ray")();
+const logger = require("../../logger");
+
+const url =
+  "https://www.allrecipes.com/recipes/22882/everyday-cooking/cookware-and-equipment/pressure-cooker/instant-pot";
+
+module.exports = () => {
+  return new Promise(resolve => {
+    x(url, ".fixed-recipe-card .grid-card-image-container", ["a@href"])(
+      (err, recipes) => {
+        if (err) {
+          logger.error(
+            `An error occurred getting a recipe from AllRecipes, error = ${
+              err.message
+            }`
+          );
+          resolve([]);
+        }
+
+        resolve(recipes);
+      }
+    );
+  });
+};
