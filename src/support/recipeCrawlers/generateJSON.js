bce34522fe0939fb9fdfe4c5958e4ece2ad15a32
Chris Legault <chrislegault2011@gmail.com>
2020-03-23 19:30:43 -0300

    Easier Recipes (#58)
    - simplify crawlers by creating static json for each source
    - move requesting and crawling logic to generateJSON instead of in each crawler

--- src/support/recipeCrawlers/generateJSON.js
+++ src/support/recipeCrawlers/generateJSON.js
@@ -1,10 +1,33 @@
 const fs = require("fs");
-const sites = require("./sites");
+const x = require("x-ray")();
+const logger = require("../../logger");
+const sites = require("./sites.json");
 
 const currentRecipes = require("./recipes.json");
 
-const promises = sites.map(site => site());
-Promise.all(promises)
+const crawlRecipes = ({ url, selector }) => {
+  return new Promise(resolve => {
+    logger.debug(`Crawling ${url} with selector ${selector}`);
+    x(url, selector, ["a@href"])((err, recipes) => {
+      if (err) {
+        logger.error(
+          `An error occurred getting a recipe from ${url}, error = ${
+            err.message
+          }`
+        );
+        resolve([]);
+      }
+
+      logger.debug(
+        `Crawling ${url} complete, found ${recipes.length} recipe(s)`
+      );
+
+      resolve(recipes);
+    });
+  });
+};
+
+Promise.all(sites.map(site => crawlRecipes(site)))
   .then(collectedRecipes => [].concat(...currentRecipes, ...collectedRecipes))
   .then(collectedRecipes => {
     collectedRecipes = [...new Set(collectedRecipes)].sort();
